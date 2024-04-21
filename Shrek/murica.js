14d757afdcf9f6ddfd48a5cc327b372c8351a548
lorenzosharks <karsonx27@lakesideschool.org>
2024-04-21 13:59:14 -0700

    fixed mcdonalds

--- Shrek/murica.js
+++ Shrek/murica.js
@@ -3,7 +3,7 @@ const images = [
     'url(https://ih0.redbubble.net/image.18720249.0440/flat,550x550,075,f.u1.jpg)',
     'url(https://www.pumpsandsystems.com/sites/default/files/PII/newsletter_images/190824/oil-barrels-pii.jpg)',
     'url(https://s.abcnews.com/images/US/la-guns2-ho-mo-20190508_hpMain_16x9_992.jpg)',
-    'url(https://upload.wikimedia.org/wikipedia/commons/3/30/McDonalds_Golden_Arches.jpg)'
+    'url(https://media.post.rvohealth.io/wp-content/uploads/2021/06/mcdonalds-1200x628-facebook.jpg)'
 ];
 
 // Function to change the background image
